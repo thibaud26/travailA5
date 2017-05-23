5d0aa12f3f59179caec997f3cd02902fb7ba7bc0
developonteur <tib26@hotmail.fr>
2017-05-23 15:34:22 +0200

    site web en travail a 5

---
+++ candidature.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+         <?php include ('head.html'); ?>
+        
+        
+</head>
+<body>
+    
+    <header>
+        
+         <h2>Candidature pour Develo'pont</h2>
+        
+    </header>
+    
+    <nav>
+        <?php include ('entete.html'); ?>
+    </nav>
+    
+    <section>
+        
+        <article>
+            
+             
+        
+                    <ul>Les inscriptions pour cette formation sont ouvertes à un public éloigné de l’emploi, mais toutes les candidatures motivées seront les bienvenues. La candidature se déroulera en deux étapes :</ul>
+
+                    <h3>Etape 1:</h3>
+
+            <ul>CANDIDATURE EN LIGNE pour vous entrainer et passer des badges sur le site, disponible en français, <a href="https://www.codecademy.com/fr" target="_blank">www.codeacademy.com</a>, Votre profil sur Codecademy, devra être public.</ul>
+
+                    <h3>Etape 2:</h3>
+
+                    <ul>ENTRETIENS DE SELECTION Si votre candidature est retenue, vous serez convoqué(e) à un entretien de sélection.</ul>
+    
+        
+        </article>
+        
+    </section>
+    
+    <aside>
+    </aside>
+    
+</body> 
+    
+    
+    
+    <Footer>
+        <?php include ('copyright.html'); ?>
+    </Footer>
+
+
+
+
+
+
+
+
+
+</html>
---
+++ contact.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+        <?php include ('head.html'); ?>
+</head>
+    
+<body>
+    
+    <header>
+        <h2>Contact</h2>
+    </header>
+    
+    <nav>
+        <?php include ('entete.html'); ?>
+    </nav>
+    
+    <section>
+        
+         <article>
+             
+                
+                <a href="https://www.facebook.com/developont/" target="_blank"> 
+                    <img class="contact" src="img/facebook.png" alt="facebook">
+                </a> 
+             
+                <a href="https://twitter.com/developont" target="_blank">
+                   <img class="contact" src="img/twitter.png" alt="twitter"> 
+                </a>
+            
+             <a href="mailto:developont@gmail.com"
+               target="_blank"> <img class="contact" src="img/mail.jpg" alt="icone mail"> 
+             </a>
+             
+        </article>
+
+    </section>
+    
+    <aside>
+    </aside> 
+     
+    
+    <Footer>
+        <?php include ('copyright.html'); ?>
+    </Footer>
+
+    
+    </body> 
+    
+
+
+
+
+
+
+
+
+</html>
