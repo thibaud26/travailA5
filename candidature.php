6374874baf6d769cc70938f58c453fc58c54c491
developonteur <tib26@hotmail.fr>
2017-05-24 14:57:59 +0200

    logo sur toutes les pages, liens sur logo, liens sur tous les 'simplon'

--- candidature.php
+++ candidature.php
@@ -9,8 +9,7 @@
 <body>
     
     <header>
-        
-         <h2>Candidature pour Develo'pont</h2>
+         <?php include ('header-logo.html'); ?>
         
     </header>
     
@@ -20,6 +19,8 @@
     
     <section>
         
+            <h2>Candidature pour Develo'pont</h2>
+        
         <article>
             
              
@@ -28,7 +29,7 @@
 
                     <h3>Etape 1:</h3>
 
-            <ul>CANDIDATURE EN LIGNE pour vous entrainer et passer des badges sur le site, disponible en français, <a href="https://www.codecademy.com/fr" target="_blank">www.codeacademy.com</a>, Votre profil sur Codecademy, devra être public.</ul>
+                    <ul>CANDIDATURE EN LIGNE pour vous entrainer et passer des badges sur le site, disponible en français, <a href="https://www.codecademy.com/fr" target="_blank">www.codeacademy.com</a>, Votre profil sur Codecademy, devra être public.</ul>
 
                     <h3>Etape 2:</h3>
 
