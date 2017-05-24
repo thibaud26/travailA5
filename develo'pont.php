a8fe472e9be8fe532c7bc2ea2f632152ff5f0aac
Alan <Alan.lazzarotto@hotmail.fr>
2017-05-24 16:00:49 +0200

    Renommage de develo'pont.php

---
+++ develo'pont.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+         <?php include ('head.html'); ?>
+         
+</head>
+<body>
+    
+    <header>
+
+        <?php include ('header-logo.html'); ?>
+        
+    </header>
+    
+    <nav>
+        <?php include ('entete.html'); ?>
+    </nav>
+    
+    <section>
+                    <h2>Qu'es que Develo'pont?</h2>
+        
+         <article>
+             
+        <p>
+            <a href="https://fr-fr.facebook.com/developont/" target="_blank">DEVELO'PONT</a> est un projet initié par l'Espace Public Numérique du Royans (EPN) et <a href="http://simplon.co/" target="_blank"><span class="span-simplon">SIMPLON.CO</span></a> .
+            
+            C'est une formation <b>GRATUITE</b>, <b>INTENSIVE</b>, <b>QUALIFIANTE</b> de 7 mois en développement web et mobile.
+
+            Notre mission est également de proposer des profils de poste adaptés aux entreprises et collectivités locales dans leur développement numérique.
+        </p>
+             <br>
+             <br>
+             
+            
+            <p><strong>Article du site: 
+            
+            <a href="http://www.memorialdelisere.fr/actualite-95355-dYvelopont-cest-parti-pour-7-mois-formation-.html" target="_blank"> Memorialdelisere.fr</a></strong></p>
+           
+            <p> A Pont-en-Royans, l’école Dévelo’Pont, initiée par l’Espace public numérique du Royans vient d’ouvrir ses portes. 20 élèves se formeront pendant 7 mois pour devenir web développeur. </p>
+
+            <p> "Accueillis gracieusement dans les locaux de l'usine Legrand, grâce à Laurent Besson, technicien du groupe et surtout bénévole de l'EPN qui, lui aussi, a fait un énorme travail dans la mise en place de la formation, les élèves ont découvert leurs locaux mis à disposition, trois espaces. En intervenant de cette façon, le groupe assure un mécénat d'entreprise plus qu'honorable que Line Rose Amozigh, présidente de l’EPN a voulu saluer lors de cette ouverture.</p>
+
+            <p> Cette formation entre dans un double dispositif : le contrat d'aide au retour à l'emploi durable (Cared), financé par la Région et POEC : préparation opérationnelle à l'emploi collectif financée par OPCALIA. Elle sera encadrée par les bénévoles de l'EPN, deux formateurs, une administratrice et une chargée des relations entreprise, tous Pontois."</p>
+            
+             <p><em>Article Publié le 03 mai 2017 à 17h32</em></p>
+             
+             
+             
+             <a href="https://fr-fr.facebook.com/developont/" target="_blank"><img class="imageDAB" src="img/develo'pontDAB.jpg" alt="image develo'pont">
+             DEVELO'PONT, promo 2017 de Develo'pont </a>
+             
+            
+        </article>
+        
+    </section>
+    
+    <aside>
+    </aside>
+    
+</body> 
+    
+    
+    
+    <Footer>
+             
+             <?php include ('copyright.html'); ?>
+        
+    </Footer>
+
+
+
+
+
+
+
+
+
+</html>
