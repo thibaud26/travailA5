6d8778039f1218a8ab71a09be9149419ccbb29b3
Alan <Alan.lazzarotto@hotmail.fr>
2017-05-29 16:33:51 +0200

    bug

---
+++ partenaire/Qui_Finance.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+        <?php include ('../php/head.html'); ?>
+        <?php include ('../php/background.php'); ?>
+    
+        <title> Develo'P<span>o</span>nt </title>  
+    
+</head>
+<body>
+    
+     <nav>
+        
+        <?php include ('../php/entete.html'); ?>
+        
+    </nav>
+    
+    <header>
+        
+        <?php include ('../php/header-logo.html'); ?> 
+        
+    </header>
+    
+    <section>
+        <div class= logo_financement1>
+                <a href="http://www.bpifrance.fr/"><img id="bpifrance" src="img/bpifrance.png" alt="BPI France"></a>
+                <a href="http://www.franceactive.org/"><img id="france_active" src="img/france_active.png" alt="France Active"></a>
+                <a href="http://www.sncf-developpement.fr/"><img id="sncf" src="img/sncf.png" alt="SNCF"></a>
+             </div>
+             <div class="logo_financement2">
+                <a href="https://europa.eu/european-union/index_fr"><img id="UE" src="img/UE.jpg" alt="UE"></a>
+                <a href="http://www.fse.gouv.fr/"><img id="fse" src="img/FSE.png" alt="FSE"></a>
+                <a href="http://www.fse.gouv.fr/"><img id="fse2" src="img/rosefse.png" alt="FSE2"></a>
+             </div>
+        
+    </section>
+    
+    <aside>
+    </aside> 
+     
+    
+    <Footer>
+        <?php include ('../php/copyright.html'); ?>
+    </Footer>
+
+    
+    </body> 
+    
+
+
+
+
+
+
+
+
+</html>
