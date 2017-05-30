269fec68b1950b0f49789db0a72ae7a7b56f023e
Stephane PACHOT <stef.pachot@gmail.com>
2017-05-30 13:40:55 +0200

    changement de lien vers page attente.php et autre

---
+++ develo'pont/c'est_ou.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+
+         <?php include ('../php/head.html'); ?>
+         <?php include ('../php/background.php'); ?>
+
+         <title>Develo'Pont</title>
+</head>
+<body>
+    
+    <nav>
+        <?php include ('../php/entete.html'); ?>
+    </nav>
+    
+    <header>
+
+        <?php include ('../php/header-logo.html'); ?>
+        
+    </header>
+
+    
+    <section>
+                    <h2>Develo'P<span>o</span>nt</h2>
+        
+         <article>
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2511.7844968861887!2d5.341486293450821!3d45.06139487697702!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x478abb82046dcbe1%3A0xce9c6af3c67f8452!2s38680+Pont-en-Royans!5e1!3m2!1sfr!2sfr!4v1496140039680" width="400" height="300" frameborder="0" style="border:0" allowfullscreen></iframe>
+        </article>
+        
+    </section>
+    
+    <aside>
+    </aside>
+    
+</body> 
+    
+    
+    
+    <Footer>
+             
+             <?php include ('../php/copyright.html'); ?>
+        
+    </Footer>
+
+
+
+
+
+
+
+
+
+</html>
