5e7437281fae8aa135bed7e9dff44d8a90ad6d93
developonteur <tib26@hotmail.fr>
2017-05-30 15:35:59 +0200

    modification et amelioration page c'est ou

--- develo'pont/cestOu.php
+++ develo'pont/cestOu.php
@@ -27,59 +27,111 @@
     
     <section>
         
-         <article>
-             <p><u>Quel est ce village ou ce déroule cette formation de developpeur Web?</u></p>
-             <p>
-                 <a class="PONTENROYANS" href="http://vercors-pontenroyans.com/" target="_blank">P<span>O</span>NT EN ROYANS</a></p>
+         <article class="pont">
+             <h3><u>Quel est ce village ou ce déroule cette formation de developpeur Web?</u></h3>
+             
+             <p><a class="PONTENROYANS" href="http://vercors-pontenroyans.com/" target="_blank">"P<span>O</span>NT EN ROYANS"</a>
+             </p>
+             
              <p>
                  <a class="LIENCESTOU" href="http://vercors-pontenroyans.com/" target="_blank">Pont en Royans</a> est une 
                  <a class="LIENCESTOU" href="https://fr.wikipedia.org/wiki/Commune_(France)" target='_blank'>commune</a> située dans le 
                  <a class="LIENCESTOU" href="https://fr.wikipedia.org/wiki/D%C3%A9partement_fran%C3%A7ais" target="_blank">departement</a> de <a class="LIENCESTOU" href="https://fr.wikipedia.org/wiki/Is%C3%A8re_(d%C3%A9partement)" target="_blank">l'Isère</a> (38) en <a class="LIENCESTOU" href="https://fr.wikipedia.org/wiki/R%C3%A9gion_fran%C3%A7aise" target="_blank">région</a> 
-                 <a class="LIENCESTOU" href="https://fr.wikipedia.org/wiki/Auvergne-Rh%C3%B4ne-Alpes">Auvergne-Rhône-Alpes</a>.</p>
+                 <a class="LIENCESTOU" href="https://fr.wikipedia.org/wiki/Auvergne-Rh%C3%B4ne-Alpes">Auvergne-Rhône-Alpes</a>.
+             </p>
              
-             <p>La commune se situe dans le Royans (Massif du Vercors).Le Royans est l'une des 7 régions naturelles du Parc naturel régional du Vercors. Le point culminant de Pont-en-Royans est le Mont-Baret (786 m d'altitude). Le village se trouve au confluent de la Bourne (source : Lans-en-Vercors) et de la Vernaison (source : Saint-Agnan-en-Vercors).</p>
+             <p>La commune se situe dans le Royans (Massif du Vercors).Le Royans est l'une des 7 régions naturelles du Parc naturel régional du Vercors. Le point culminant de Pont-en-Royans est le Mont-Baret (786 m d'altitude). Le village se trouve au confluent de la Bourne (source : Lans-en-Vercors) et de la Vernaison (source : Saint-Agnan-en-Vercors).
+             </p>
              
         </article>
              
              
-        <article>
+        <article class="histoire">
             
-            <p><b>Un peu d'histoire</b></p>
+            <h3>Un peu d'histoire</h3>
             
             <p>Le pont : au départ c'est un passage étroit jeté entre deux rochers qui permet (point d'entrée) l'accès aux plateaux du massif du Vercors, et de traverser le torrent de la Bourne dont les gorges sont abruptes et très étroites. La première citation désigne un certain pont Chochignon (relire abbé Fillet, et S. Chaussamy). Le pont est étroit, tout au plus trois mètres, et son entrée démarre à la hauteur de la maison Thomas sur l'actuel Pont Picard (on ignore l'origine de ce nom, qui est donc moderne ; on parle aussi du gouffre Picard, creux dans la Bourne profond d'une dizaine de mètres à peu près sous le pont).
+            </p>
               
                 <img class="imagepont1800" src="../img/ponten1800.jpg" alt="image pont en royans en 1800">
 
 
-            <p>Au XIXe siècle le Pont est élargi. La route de Villard est créée en 1872, la route de sainte Eulalie actuelle est créée elle aussi avant la fin du XIXe siècle, et la circulation des charrois de troncs de bois provenant de la Route des Goulets entraine un besoin d'élargissement. De nombreuses maisons sont abattues (il y avait avant trois fois plus de maisons suspendues au-dessus du gouffre qu'au XXIe siècle).</p>
+            <p>Au XIXe siècle le Pont est élargi. La route de Villard est créée en 1872, la route de sainte Eulalie actuelle est créée elle aussi avant la fin du XIXe siècle, et la circulation des charrois de troncs de bois provenant de la Route des Goulets entraine un besoin d'élargissement. De nombreuses maisons sont abattues (il y avait avant trois fois plus de maisons suspendues au-dessus du gouffre qu'au XXIe siècle).
+            </p>
 
-            <p>Pont-en-Royans est un bourg touristique riche en histoire. On apprend aux archives du Dauphiné qu'il y a eu trois châteaux : Le premier château dans l'appellation topographique ancienne correspond au bourg fortifié. Le deuxième château à l'actuel vestige au-dessus de la "montagne trois châteaux". Le troisième château des XVIIe et XVIIIe siècles fut détruit lors d'un bombardement en 1945 et correspond à l'emplacement du collège. Ses remparts, dont il ne reste que de rares vestiges, incluent « La porte de France », « la porte Agnès », « la porte de Villeneuve », et « la porte du Merle ». C’est là que s’arrêtaient les limites du bourg en des temps anciens, et si les toits de pierres blanches ont disparu, le village et ses maisons suspendues, accrochées au rocher, attirent toujours les touristes en été sur les bords de la rivière, la Bourne.</p>
+            <p>Pont-en-Royans est un bourg touristique riche en histoire. On apprend aux archives du Dauphiné qu'il y a eu trois châteaux : Le premier château dans l'appellation topographique ancienne correspond au bourg fortifié. Le deuxième château à l'actuel vestige au-dessus de la "montagne trois châteaux". Le troisième château des XVIIe et XVIIIe siècles fut détruit lors d'un bombardement en 1945 et correspond à l'emplacement du collège. Ses remparts, dont il ne reste que de rares vestiges, incluent « La porte de France », « la porte Agnès », « la porte de Villeneuve », et « la porte du Merle ». C’est là que s’arrêtaient les limites du bourg en des temps anciens, et si les toits de pierres blanches ont disparu, le village et ses maisons suspendues, accrochées au rocher, attirent toujours les touristes en été sur les bords de la rivière, la Bourne.
+            </p>
                 
                 <img class="imagemaisonsuspendu" src="../img/maisonsuspendu.jpg" alt="image maison suspendu de pont en royans">
 
 
-            <p>Au XIXe siècle, ce bourg fut sous Napoléon III plus connu que la ville de Valence, pourtant proche. Sa notoriété dépassa largement les frontières de France, grâce aux routes creusées dans la roche karstique du Vercors (Un exploit pour l'époque qui contribua largement à la renommée du savoir-faire du génie français, lors des toutes premières expositions universelles à Paris).</p>
+            <p>Au XIXe siècle, ce bourg fut sous Napoléon III plus connu que la ville de Valence, pourtant proche. Sa notoriété dépassa largement les frontières de France, grâce aux routes creusées dans la roche karstique du Vercors (Un exploit pour l'époque qui contribua largement à la renommée du savoir-faire du génie français, lors des toutes premières expositions universelles à Paris).
+            </p>
 
 
-            <P>Pont-en-Royans fut aussi l'une des trois premières villes (pour le nombre d'habitants) électrifiées de France. La conduite d'eau forcée passe encore sous la ville (un musée de l'eau permet de la découvrir).</P>
+            <P>Pont-en-Royans fut aussi l'une des trois premières villes (pour le nombre d'habitants) électrifiées de France. La conduite d'eau forcée passe encore sous la ville (un musée de l'eau permet de la découvrir).
+            </P>
 
 
-            <p>On fabriquait encore dans les années 1950 du matériel électrique pour l'habitat : interrupteurs, douilles... (À l'origine tous ces équipements étaient tournés en bois de buis* dans lesquels on plaçait le système en cuivre).</p>
+            <p>On fabriquait encore dans les années 1950 du matériel électrique pour l'habitat : interrupteurs, douilles... (À l'origine tous ces équipements étaient tournés en bois de buis* dans lesquels on plaçait le système en cuivre).
+            </p>
 
     
-            <p>Le tournage sur bois de buis fut longtemps une des activités principales de la ville.</p>
+            <p>Le tournage sur bois de buis fut longtemps une des activités principales de la ville.
+            </p>
 
 
-            <p>On redécouvre de nos jours dans de très nombreux livres des XIXe et XXe siècles, les premières photos sur plaques en "verre photographique lumière" qui représentent les maisons suspendues de Pont-en-Royans et les routes du Vercors (routes des Grands Goulets, route des gorges de la Bourne, Pont de la Goule Noire..).</p>
+            <p>On redécouvre de nos jours dans de très nombreux livres des XIXe et XXe siècles, les premières photos sur plaques en "verre photographique lumière" qui représentent les maisons suspendues de Pont-en-Royans et les routes du Vercors (routes des Grands Goulets, route des gorges de la Bourne, Pont de la Goule Noire..).
+            </p>
 
 
-            <p>Pendant la Seconde Guerre mondiale la place du Pont Picard fut l'une des plaques tournantes des mouvements résistants.</p>
+            <p>Pendant la Seconde Guerre mondiale la place du Pont Picard fut l'une des plaques tournantes des mouvements résistants.
+            </p>
         
         </article>
         
-        <article>
-             <p><B>Sites géologiques remarquables</B></p>
+        <article class="sitegeologiques">
+            <h3>Sites géologiques remarquables</h3>
+            
              <p>La « reculée des Grands Goulets » est un site géologique remarquable de 1 645,95 hectares, parcouru par la Vernaison, qui se trouve sur les communes de Châtelus (au lieu-dit Grands Goulets), La Chapelle-en-Vercors, Échevis, Sainte-Eulalie-en-Royans, Saint-Julien-en-Vercors, Saint-Laurent-en-Royans, Saint-Martin-en-Vercors et Pont-en-Royans. En 2014, elle est classée à l'« Inventaire du patrimoine géologique »1.</p>
+            
+        </article>
+        
+        <article class="monuments">
+            
+           <h3>Lieux et monuments</h3>
+            
+            <img class="clochet" src="../img/clochet.jpg" alt="image du clochet">
+            
+            <p>Aujourd'hui, Pont-en-Royans est un village à l'architecture médiévale du XVIe siècle, célèbre pour ses maisons colorées et suspendues à flanc de vallée. Son architecture initiale est due à une ingénieuse adaptation du village à l'environnement afin de favoriser son activité de négoce du bois.
+            </p>
+            
+            <h3>Patrimoine culturel</h3>
+            
+            <img class="museedeleau" src="../img/museedeleau.jpg" alt="image du musée de l'eau">
+            
+            <p>Le musée de l'eau propose d'explorer l'eau dans tous ses états.
+            Un bar à eau permet de goûter à plus de 900 eaux du monde en bouteilles.
+            Le centre d'art de la Halle présente des expositions d'art actuel.
+            </p>
+                
+            <h3>Patrimoine naturel</h3>
+            
+            <img class="grotte" src="../img/grotte.jpg" alt="image grotte de choranche">
+            
+            <p>
+            Les grottes de Choranche permettent de découvrir une rivière souterraine, des stalactites uniques, et des batraciens (des protées) rescapés de l'époque des dinosaures.
+            Le cirque de Choranche et la cascade du Gournier.
+            Gorges de la Bourne : 
+                
+            <img class="gorgebourne" src="../img/gorgebourne.jpg" alt="image des gorges de la bourne">
+                
+            la route des gorges de la Bourne entre Pont-en-Royans et Villard-de-Lans, route à encorbellement taillée dans la roche et classée 3 étoiles dans le guide Michelin. Des travaux y sont entrepris depuis 2008 afin de sécuriser ce tracé à l'origine de nombreux accidents8.
+            </p>
+            
+            
+
+        
         </article>
     
     </section>
