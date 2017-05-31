cc00f5f6395949e9bbbe6f9c2f28ed994d9da480
jalcaraz <alcaraz.joffrey@gmail.com>
2017-05-31 22:40:44 +0200

    ajout de la section admin

---
+++ admin/admin.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <?php include ('../php/head.html'); ?>
+    <?php include ('../php/background.php'); ?>
+    <title>Admin</title>  
+    
+    
+    <link href='https://fonts.googleapis.com/css?family=Josefin+Sans' rel='stylesheet' type='text/css'>
+    
+</head>
+    
+<body>
+    
+    
+    
+    <nav>
+        <?php include ('../php/entete.html'); ?>
+    </nav>
+    
+    <header>
+        <?php include ('../php/header-logo.html'); ?> 
+    </header>
+    
+    <section>
+        
+         <article>
+             <h1>Page admin</h1>
+             <p>Top Secret!</p>
+        </article>
+    
+    </section>
+    
+    <aside>
+    </aside> 
+     
+    
+    <Footer>
+    </Footer>
+
+    
+    </body> 
+    
+
+
+
+
+
+
+
+
+</html>
