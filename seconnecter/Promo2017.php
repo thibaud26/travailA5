f6326cfe232e7a4f7d0820637c59c043da3b30af
Alan <Alan.lazzarotto@hotmail.fr>
2017-05-31 09:52:08 +0200

    Ajout du dossier 'Photos' et création de la base de la page 'Promo 2017'

---
+++ seconnecter/Promo2017.php
@@ -0,0 +1,166 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Develo'Pont</title>
+    <?php include ('../php/head.html'); ?>
+    <?php include ('../php/background.php'); ?> 
+</head>
+    
+<body>
+    
+    <nav>
+        <?php include ('../php/entete.html'); ?>
+    </nav>
+    
+    <header>
+        <?php include ('../php/header-logo.html'); ?>
+    </header>
+    
+    <section>
+        <article>
+            <ul>
+                <li>
+                    <a href="#">
+                    <img src="../Photos/Joffrey.jpg" alt="Joffrey">
+                    <strong>Joffrey</strong>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                    <img src="../Photos/Thibaud.jpg" alt="Thibaud">
+                    <strong>Thibaud</strong>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                    <img src="../Photos/Fanny.jpg" alt="Fanny">
+                    <strong>Fanny</strong>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                    <img src="../Photos/Alan.jpg" alt="Alan">
+                    <strong>Alan</strong>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                    <img src="../Photos/Stephane.jpg" alt="Stéphane">
+                    <strong>Stéphane</strong>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                    <img src="../Photos/BenoitLi.jpg" alt="BenoitLi">
+                    <strong>BenoitLi</strong>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                    <img src="../Photos/BenoitLa.jpg" alt="BenoitLa">
+                    <strong>BenoitLa</strong>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                    <img src="../Photos/Fred.jpg" alt="Fred">
+                    <strong>Fred</strong>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                    <img src="../Photos/Jordan.jpg" alt="Jordan">
+                    <strong>Jordan</strong>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                    <img src="../Photos/Kevin.jpg" alt="Kevin">
+                    <strong>Kevin</strong>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                    <img src="../Photos/Lise-Claire.jpg" alt="Lise-Claire">
+                    <strong>Lise-Claire</strong>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                    <img src="../Photos/Mehdi.jpg" alt="Mehdi">
+                    <strong>Mehdi</strong>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                    <img src="../Photos/Mourad.jpg" alt="Mourad">
+                    <strong>Mourad</strong>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                    <img src="../Photos/Piotr.jpg" alt="Piotr">
+                    <strong>Piotr</strong>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                    <img src="../Photos/Nolwenn.jpg" alt="Nolwenn">
+                    <strong>Nolwenn</strong>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                    <img src="../Photos/Mylene.jpg" alt="Mylene">
+                    <strong>Mylene</strong>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                    <img src="../Photos/Nima.jpg" alt="Nima">
+                    <strong>Nima</strong>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                    <img src="../Photos/Quentin.jpg" alt="Quentin">
+                    <strong>Quentin</strong>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                    <img src="../Photos/Rita.jpg" alt="Rita">
+                    <strong>Rita</strong>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                    <img src="../Photos/Sylvain.jpg" alt="Sylvain">
+                    <strong>Sylvain</strong>
+                    </a>
+                </li>
+            </ul>
+        </article>
+    </section>
+    
+    <aside>
+    </aside> 
+     
+    
+    <Footer>
+        <?php include ('../php/copyright.html'); ?>
+    </Footer>
+
+    
+</body> 
+    
+
+
+
+
+
+
+
+
+</html>
