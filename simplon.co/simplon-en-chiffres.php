f5e0586fba47a681fffae919d522628795a14602
developonteur <tib26@hotmail.fr>
2017-05-31 12:13:33 +0200

    debut de creation de la page simplon en chiffres

---
+++ simplon.co/simplon-en-chiffres.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+            <?php include ('../php/head.html'); ?>
+            <?php include ('../php/background.php'); ?>
+         <title>Simplon.co en chiffres</title>
+</head>
+    
+<body>
+    
+    <nav>
+            <?php include ('../php/entete.html'); ?>
+    </nav>
+    
+    <header>
+            <?php include ('../php/header-logo.html'); ?>
+    </header>
+    
+    
+    <section>
+        
+        
+         <article> 
+  
+             <h2>Les chiffres sur Simplon.co</h2>
+      
+
+        </article>
+    
+    </section>
+    
+    <aside>
+    </aside> 
+     
+    
+    <Footer>
+        
+        <a href="http://simplon.co/" target="_blank"><img class="simplon" src="../img/logosimplon.jpg" target="_blank"></a>
+        <?php include ('../php/copyright.html'); ?>
+        
+    </Footer>
+
+    
+    </body> 
+    
+
+
+
+
+
+
+
+
+</html>
