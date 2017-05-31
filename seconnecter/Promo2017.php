299ab31092ab43fec71e15a1cbf4d28e8644b977
Alan <Alan.lazzarotto@hotmail.fr>
2017-05-31 11:16:20 +0200

    Mise en page de 'Promo 2017'

--- seconnecter/Promo2017.php
+++ seconnecter/Promo2017.php
@@ -19,123 +19,123 @@
     
     <section>
         <article>
-            <ul>
+            <ul class="galerie">
                 <li>
-                    <a href="#">
+                    <a>
                     <img src="../Photos/Joffrey.jpg" alt="Joffrey">
                     <strong>Joffrey</strong>
                     </a>
                 </li>
                 <li>
-                    <a href="#">
+                    <a>
                     <img src="../Photos/Thibaud.jpg" alt="Thibaud">
                     <strong>Thibaud</strong>
                     </a>
                 </li>
                 <li>
-                    <a href="#">
+                    <a>
                     <img src="../Photos/Fanny.jpg" alt="Fanny">
                     <strong>Fanny</strong>
                     </a>
                 </li>
                 <li>
-                    <a href="#">
+                    <a>
                     <img src="../Photos/Alan.jpg" alt="Alan">
                     <strong>Alan</strong>
                     </a>
                 </li>
                 <li>
-                    <a href="#">
+                    <a>
                     <img src="../Photos/Stephane.jpg" alt="Stéphane">
                     <strong>Stéphane</strong>
                     </a>
                 </li>
                 <li>
-                    <a href="#">
+                    <a>
                     <img src="../Photos/BenoitLi.jpg" alt="BenoitLi">
                     <strong>BenoitLi</strong>
                     </a>
                 </li>
                 <li>
-                    <a href="#">
+                    <a>
                     <img src="../Photos/BenoitLa.jpg" alt="BenoitLa">
                     <strong>BenoitLa</strong>
                     </a>
                 </li>
                 <li>
-                    <a href="#">
+                    <a>
                     <img src="../Photos/Fred.jpg" alt="Fred">
                     <strong>Fred</strong>
                     </a>
                 </li>
                 <li>
-                    <a href="#">
+                    <a>
                     <img src="../Photos/Jordan.jpg" alt="Jordan">
                     <strong>Jordan</strong>
                     </a>
                 </li>
                 <li>
-                    <a href="#">
+                    <a>
                     <img src="../Photos/Kevin.jpg" alt="Kevin">
                     <strong>Kevin</strong>
                     </a>
                 </li>
                 <li>
-                    <a href="#">
+                    <a>
                     <img src="../Photos/Lise-Claire.jpg" alt="Lise-Claire">
                     <strong>Lise-Claire</strong>
                     </a>
                 </li>
                 <li>
-                    <a href="#">
+                    <a>
                     <img src="../Photos/Mehdi.jpg" alt="Mehdi">
                     <strong>Mehdi</strong>
                     </a>
                 </li>
                 <li>
-                    <a href="#">
+                    <a>
                     <img src="../Photos/Mourad.jpg" alt="Mourad">
                     <strong>Mourad</strong>
                     </a>
                 </li>
                 <li>
-                    <a href="#">
+                    <a>
                     <img src="../Photos/Piotr.jpg" alt="Piotr">
                     <strong>Piotr</strong>
                     </a>
                 </li>
                 <li>
-                    <a href="#">
+                    <a>
                     <img src="../Photos/Nolwenn.jpg" alt="Nolwenn">
                     <strong>Nolwenn</strong>
                     </a>
                 </li>
                 <li>
-                    <a href="#">
+                    <a>
                     <img src="../Photos/Mylene.jpg" alt="Mylene">
                     <strong>Mylene</strong>
                     </a>
                 </li>
                 <li>
-                    <a href="#">
+                    <a>
                     <img src="../Photos/Nima.jpg" alt="Nima">
                     <strong>Nima</strong>
                     </a>
                 </li>
                 <li>
-                    <a href="#">
+                    <a>
                     <img src="../Photos/Quentin.jpg" alt="Quentin">
                     <strong>Quentin</strong>
                     </a>
                 </li>
                 <li>
-                    <a href="#">
+                    <a>
                     <img src="../Photos/Rita.jpg" alt="Rita">
                     <strong>Rita</strong>
                     </a>
                 </li>
                 <li>
-                    <a href="#">
+                    <a>
                     <img src="../Photos/Sylvain.jpg" alt="Sylvain">
                     <strong>Sylvain</strong>
                     </a>
