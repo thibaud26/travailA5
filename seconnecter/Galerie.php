879c80a130bf91dfdd4d37057510957789ecd964
fanny26 <fannysterieuse2@hotmail.fr>
2017-06-01 16:51:27 +0200

    galerie photo

---
+++ seconnecter/Galerie.php
@@ -0,0 +1,99 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    
+    <?php include ('../php/head.html'); ?>
+    <?php include ('../php/background.php'); ?>
+    <link href='https://fonts.googleapis.com/css?family=Josefin+Sans' rel='stylesheet' type='text/css'>
+    
+    <title></title>
+    
+</head>
+    
+<body>
+    
+    <nav>
+        
+        <?php include ('../php/entete.html'); ?>
+        
+    </nav>
+    
+    <header>
+        
+        <?php include ('../php/header-logo.html'); ?> 
+        
+    </header>
+      
+    <section>
+        
+        <article>
+            
+             <div class='galeriePhoto'>
+                 <input id="play" type="checkbox">
+                 <label for="play">Lancer Le Diapo</label>
+                    <div class="contenu_galerie">
+                        <div class="galerie">
+                            <img src="../Photos/1.jpg" alt>
+                            <img src="../Photos/2.jpg" alt>
+                            <img src="../Photos/3.jpg" alt>
+                            <img src="../Photos/4.jpg" alt>
+                            <img src="../Photos/5.jpg" alt>
+                            <img src="../Photos/6.jpg" alt>
+                            <img src="../Photos/7.jpg" alt>
+                            <img src="../Photos/8.jpg" alt>
+                            <img src="../Photos/9.jpg" alt>
+                            <img src="../Photos/10.jpg" alt>
+                            <img src="../Photos/11.jpg" alt>
+                            <img src="../Photos/12.jpg" alt>
+                            <img src="../Photos/13.jpg" alt>
+                            <img src="../Photos/14.jpg" alt>
+                            <img src="../Photos/15.jpg" alt>
+                            <img src="../Photos/16.jpg" alt>
+                            <img src="../Photos/17.jpg" alt>
+                            <img src="../Photos/18.jpg" alt>
+                            <img src="../Photos/19.jpg" alt>
+                            <img src="../Photos/1.jpg" alt>
+                            <img src="../Photos/2.jpg" alt>
+                            <img src="../Photos/3.jpg" alt>
+                            <img src="../Photos/4.jpg" alt>
+                            <img src="../Photos/5.jpg" alt>
+                            <img src="../Photos/6.jpg" alt>
+                            <img src="../Photos/7.jpg" alt>
+                            <img src="../Photos/8.jpg" alt>
+                            <img src="../Photos/9.jpg" alt>
+                            <img src="../Photos/10.jpg" alt>
+                            <img src="../Photos/11.jpg" alt>
+                            <img src="../Photos/12.jpg" alt>
+                            <img src="../Photos/13.jpg" alt>
+                            <img src="../Photos/14.jpg" alt>
+                            <img src="../Photos/15.jpg" alt>
+                            <img src="../Photos/16.jpg" alt>
+                            <img src="../Photos/17.jpg" alt>
+                            <img src="../Photos/18.jpg" alt>
+                            <img src="../Photos/19.jpg" alt>
+                        </div>
+                    </div>
+            </div>
+        </article>
+    
+    </section>
+    
+    <aside>
+        
+        <?php include ('../php/reseauxsociaux.html'); ?> 
+        
+    </aside> 
+     
+    
+    <Footer>
+        
+        <?php include ('../php/copyright.html'); ?>
+        
+    </Footer>
+
+    
+    </body> 
+    
+
+</html>
