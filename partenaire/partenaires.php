76dca9d78163d97933f994aebbc5b1191eb02e15
Alan <Alan.lazzarotto@hotmail.fr>
2017-06-01 14:54:50 +0200

    Ajout de l'EPN sur la page 'partenaires'

--- partenaire/partenaires.php
+++ partenaire/partenaires.php
@@ -29,32 +29,42 @@
     <section>
         
          <article>
-             <div class= logo_part1>
-                 <a href="http://cc-royans-vercors.org/fr_FR/index.php" target="_blank">
-                     <img id="royans" src="../img/royans.png" alt="Sud Gresivaudan"></a>
+            <div class= logo_part1>
+                <a href="http://cc-royans-vercors.org/fr_FR/index.php" target="_blank">
+                     <img id="royans" src="../img/royans.png" alt="Sud Gresivaudan">
+                </a>
                      
                 <a href="https://fr.wikipedia.org/wiki/Liaison_entre_actions_de_d%C3%A9veloppement_de_l%27%C3%A9conomie_rurale" target="_blank">
-                    <img id="leader" src="../img/leader.png" alt="Leader"></a>
+                    <img id="leader" src="../img/leader.png" alt="Leader">
+                </a>
                      
                 <a href="http://www.vercors.org/" target="_blank">
-                    <img id="vercors" src="../img/vercors.png" alt="Vercors"></a>
+                    <img id="vercors" src="../img/vercors.png" alt="Vercors">
+                </a>
                      
                 <a href="http://www.vercors.org/" target="_blank">
-                    <img id="massif" src="../img/massif.png" alt="Massif"></a>
-                     
+                    <img id="massif" src="../img/massif.png" alt="Massif">
+                </a>
              </div>
              
              <div class="logo_part2">
                  
-                 <a href="http://www.emploi-pvsg.org/" target="_blank">
-                     <img id="emploi" src="../img/emploi.png" alt="Emploi"></a>
+                <a href="http://www.emploi-pvsg.org/" target="_blank">
+                     <img id="emploi" src="../img/emploi.png" alt="Emploi">
+                </a>
                  
-                 <a href="https://www.legrand.fr/" target="_blank">
-                     <img id="legrand" src="../img/legrand.png" alt="LeGrand"></a>
+                <a href="https://www.legrand.fr/" target="_blank">
+                     <img id="legrand" src="../img/legrand.png" alt="LeGrand">
+                </a>
                  
-                 <a href="http://meef26.org/" target="_blank">
-                     <img id="formation" src="../img/formation.png" alt="Formation"></a>
-                 
+                <a href="http://meef26.org/" target="_blank">
+                     <img id="formation" src="../img/formation.png" alt="Formation">
+                </a>
+             </div>
+             <div class="logo_part3">
+                <a href="https://www.epn-du-royans.fr/" target="_blank">
+                     <img id="EPN" src="../img/EPN.png" alt="EPN">
+                </a>
              </div>
         </article>
     
