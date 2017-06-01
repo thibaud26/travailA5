abc26bc11669e6ad9e4c594b39839b2c52cd2ea6
developonteur <tib26@hotmail.fr>
2017-06-01 09:54:54 +0200

    ajout des liens partenaires

--- partenaire/partenaires.php
+++ partenaire/partenaires.php
@@ -30,15 +30,31 @@
         
          <article>
              <div class= logo_part1>
-                <img id="royans" src="../img/royans.png" alt="Sud Gresivaudan" target="_blank">
-                <img id="leader" src="../img/leader.png" alt="Leader">
-                <img id="vercors" src="../img/vercors.png" alt="Vercors">
-                <img id="massif" src="../img/massif.png" alt="Massif">
+                 <a href="http://cc-royans-vercors.org/fr_FR/index.php" target="_blank">
+                     <img id="royans" src="../img/royans.png" alt="Sud Gresivaudan"></a>
+                     
+                <a href="https://fr.wikipedia.org/wiki/Liaison_entre_actions_de_d%C3%A9veloppement_de_l%27%C3%A9conomie_rurale" target="_blank">
+                    <img id="leader" src="../img/leader.png" alt="Leader"></a>
+                     
+                <a href="http://www.vercors.org/" target="_blank">
+                    <img id="vercors" src="../img/vercors.png" alt="Vercors"></a>
+                     
+                <a href="http://www.vercors.org/" target="_blank">
+                    <img id="massif" src="../img/massif.png" alt="Massif"></a>
+                     
              </div>
+             
              <div class="logo_part2">
-                <img id="emploi" src="../img/emploi.png" alt="Emploi">
-                <img id="legrand" src="../img/legrand.png" alt="LeGrand">
-                <img id="formation" src="../img/formation.png" alt="Formation">
+                 
+                 <a href="http://www.emploi-pvsg.org/" target="_blank">
+                     <img id="emploi" src="../img/emploi.png" alt="Emploi"></a>
+                 
+                 <a href="https://www.legrand.fr/" target="_blank">
+                     <img id="legrand" src="../img/legrand.png" alt="LeGrand"></a>
+                 
+                 <a href="http://meef26.org/" target="_blank">
+                     <img id="formation" src="../img/formation.png" alt="Formation"></a>
+                 
              </div>
         </article>
     
