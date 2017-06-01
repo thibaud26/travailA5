8077a698531d425009beb794f02c9db850374544
joffreyal <alcaraz.joffrey@gmail.com>
2017-06-01 21:24:31 +0200

    rangement css et html

--- accueil/accueil.php
+++ accueil/accueil.php
@@ -1,20 +1,19 @@
 <!DOCTYPE html>
 <html>
 
+    <html lang="fr">
+
 <head>
         <?php include ('../php/head.html'); ?>
         <?php include ('../php/background.php'); ?>
     
         <title>Develo'Pont</title>  
     
-    
-    <link href='https://fonts.googleapis.com/css?family=Josefin+Sans' rel='stylesheet' type='text/css'>
+        <link href='https://fonts.googleapis.com/css?family=Josefin+Sans' rel='stylesheet' type='text/css'>
 
-
-    
 </head>
-<body>
     
+<body>
     
      <nav>
         
@@ -28,17 +27,14 @@
         
     </header>
     
-    
-
-    
     <link href='https://fonts.googleapis.com/css?family=Josefin+Sans' rel='stylesheet' type='main.css'>
 
     <section>
-        
-        
-        
-     <article> 
+           
+        <article>
+            
             <h2>La formation developpeur web a <a href="http://simplon.co/" target="_blank"><span class="span-simplon">SIMPL<span>O</span>N.CO</span></a> c'est quoi?</h2>
+            
             <ul>
                 <li>Apprendre à créer des sites Internet et des applications web, et en faire son métier</li>
                 <li>En intensif, sur un cycle de 6 mois</li>
@@ -52,41 +48,39 @@
         </article>
         
       
-           <article>
+        <article>
         
             <h2> Develo'P<span>o</span>nt a pont en royans</h2>
         
                 <p>Pour la première fois en Isère, mais aussi la première fois dans un petit village authentique adossé au Vercors, Simplon.co propose une formation en développement web, en PHP, gratuite et intensive, de 7 mois à partir du mois d’avril 2017.</p>
-                Mené dans une démarche d’innovation sociale, ce projet constitue un double défi :
+            
+                <p>Mené dans une démarche d’innovation sociale, ce projet constitue un double défi :</p>
+            
             <ul>
                 <li> économique : participer à l’endiguement du chômage en répondant aux besoins de recrutement des entreprises du numérique engagées dans cette démarche citoyenne,</li>
                 <li> social : en agissant sur l’emploi et la qualification de publics marginalisés sélectionnés uniquement sur la base de critères sociaux et de leur motivation, et non selon les diplômes.</li>
             </ul>
-                <a id="candidature" href="../simplon.co/candidature.php">Dép<span>o</span>sez votre candidature!</a>    
+            
+            <a id="candidature" href="../simplon.co/candidature.php">Dép<span>o</span>sez votre candidature!</a>
+            
         </article>
         
            
         
     </section>
-      <?php include ('../php/reseauxsociaux.html'); ?> 
+    
+        <?php include ('../php/reseauxsociaux.html'); ?> 
 
     <aside>
-
-
-        
+   
     </aside>
     
-</body> 
-    
-    
-    
     <footer>
-        
         
         <?php include ('../php/copyright.html'); ?>
        
     </footer>
-
-
-
+    
+</body> 
+    
 </html>
