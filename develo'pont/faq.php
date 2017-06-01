4a760f135947dde4bd9e050561b7b077ef1319c4
jalcaraz <alcaraz.joffrey@gmail.com>
2017-06-01 18:33:21 +0200

    Ajout page FAQ

---
+++ develo'pont/faq.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <?php include ('../php/head.html'); ?>
+    <?php include ('../php/background.php'); ?>
+    
+    <title>Develo'Pont</title>  
+    
+    <link href='https://fonts.googleapis.com/css?family=Josefin+Sans' rel='stylesheet' type='text/css'>
+    
+</head>
+    
+<body>
+   
+    <nav>
+        
+        <?php include ('../php/entete.html'); ?>
+        
+    </nav>
+       
+    <header>
+            
+        <?php include ('../php/header-logo.html'); ?> 
+        
+    </header>
+    
+    <section>
+        
+        <h2>Foire aux questions</h2>
+        
+        <article>
+            
+            <h2>Qui peut candidater ?</h2>
+            
+            <p>Develo'pont est ouvert à tous ! Même les débutants.</p>
+            
+        </article>
+        
+        <article>
+            
+            <h2>Suspendisse quam ipsum ?</h2>
+            
+            <p>Suspendisse quam ipsum, feugiat id lacinia nec, hendrerit ac lacus. Sed quis mollis nisl, et faucibus nulla. Aenean aliquet id ex in finibus. Nulla faucibus pulvinar ipsum, et aliquet mi malesuada lobortis. Aenean elit massa, elementum vitae convallis in, vehicula vel mi. Vestibulum et dolor sed ante aliquam dignissim. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nam vehicula id risus eget convallis. Aliquam quis massa sodales mauris tempus tempor. Donec scelerisque pellentesque justo vitae vestibulum.</p>
+            
+        </article> 
+        
+        <article>
+            
+            <h2>Suspendisse quam ipsum ?</h2>
+            
+            <p>Suspendisse quam ipsum, feugiat id lacinia nec, hendrerit ac lacus. Sed quis mollis nisl, et faucibus nulla. Aenean aliquet id ex in finibus. Nulla faucibus pulvinar ipsum, et aliquet mi malesuada lobortis. Aenean elit massa, elementum vitae convallis in, vehicula vel mi. Vestibulum et dolor sed ante aliquam dignissim. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nam vehicula id risus eget convallis. Aliquam quis massa sodales mauris tempus tempor. Donec scelerisque pellentesque justo vitae vestibulum.</p>
+            
+        </article> 
+        
+        <article>
+            
+            <h2>Suspendisse quam ipsum ?</h2>
+            
+            <p>Suspendisse quam ipsum, feugiat id lacinia nec, hendrerit ac lacus. Sed quis mollis nisl, et faucibus nulla. Aenean aliquet id ex in finibus. Nulla faucibus pulvinar ipsum, et aliquet mi malesuada lobortis. Aenean elit massa, elementum vitae convallis in, vehicula vel mi. Vestibulum et dolor sed ante aliquam dignissim. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nam vehicula id risus eget convallis. Aliquam quis massa sodales mauris tempus tempor. Donec scelerisque pellentesque justo vitae vestibulum.</p>
+            
+        </article> 
+    
+    </section>
+    
+        <?php include ('../php/reseauxsociaux.html'); ?> 
+    
+    <aside>
+    </aside> 
+     
+    
+    <Footer>
+        
+        <?php include ('../php/copyright.html'); ?>
+        
+    </Footer>
+
+    
+    </body> 
+    
+
+</html>
