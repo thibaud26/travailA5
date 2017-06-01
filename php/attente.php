677bc52ac623d7797d2146ff982803c67a4b5849
Stephane PACHOT <stef.pachot@gmail.com>
2017-06-01 09:53:28 +0200

    maj

--- php/attente.php
+++ php/attente.php
@@ -26,6 +26,12 @@
         
          <article>
             <p id='attente'>cette page est en construction<p>
+            <p class="dd">
+            <img src="https://media.giphy.com/media/nIUav1AOaK06s/giphy.gif">
+            <img src="https://media.giphy.com/media/Sx6mNBsKpRoRy/giphy.gif">
+            <img src="https://media.giphy.com/media/eDpmSnSvWS2EU/giphy.gif">
+            <img src="https://media.giphy.com/media/ZTans30ONaaIM/giphy.gif">
+            </p>
         </article>
         
     </section>
