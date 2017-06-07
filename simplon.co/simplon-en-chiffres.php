f191923e40b92278c0e55e6de0d17e3962aabb2f
Alan <Alan.lazzarotto@hotmail.fr>
2017-06-07 16:27:11 +0200

    Ajout d'une classe sur les <strong> de la page 'Simplon-en-chiffres'

--- simplon.co/simplon-en-chiffres.php
+++ simplon.co/simplon-en-chiffres.php
@@ -41,15 +41,15 @@
                 <div class="chiffresImpair">
                     
                     <div id="progress1">
-                        <p>Salariés:<br><strong value="0" min="0" max="75">75</strong></p>
+                        <p>Salariés:<br><strong class="valeur" value="0" min="0" max="75">75</strong></p>
                     </div>
              
                     <div id="progress3">
-                        <p>Féminisation équipe<br><strong value="0" min="0" max="42">42%</strong></p>
+                        <p>Féminisation équipe<br><strong class="valeur" value="0" min="0" max="42">42%</strong></p>
                     </div>
              
                     <div id="progress5">
-                        <p>Fabriques à l'étranger<br><strong value="0" min="0" max="4">4</strong></p>
+                        <p>Fabriques à l'étranger<br><strong class="valeur" value="0" min="0" max="4">4</strong></p>
                     </div>
                         
                 </div>
@@ -58,15 +58,15 @@
                 <div class="chiffresPair">
                     
                     <div id="progress2">
-                        <p>Simplonien(ne)s dans l'équipe<br><strong value="0" min="0" max="19">19%</strong></p>
+                        <p>Simplonien(ne)s dans l'équipe<br><strong class="valeur" value="0" min="0" max="19">19%</strong></p>
                     </div>
 
                     <div id="progress4">
-                        <p>Fabriques en France<br><strong value="0" min="0" max="30">30</strong></p>
+                        <p>Fabriques en France<br><strong class="valeur" value="0" min="0" max="30">30</strong></p>
                     </div>
 
                     <div id="progress6">
-                        <p>Fabriques à ouvrir<br><strong value="0" min="0" max="13">13</strong></p>
+                        <p>Fabriques à ouvrir<br><strong class="valeur" value="0" min="0" max="13">13</strong></p>
                     </div>
                     
                 </div>
@@ -81,19 +81,19 @@
                 <div class="chiffresImpair">
                     
                     <div id="progress7">
-                        <p>Personnes formées<br><strong value="0" min="1" max="779">779</strong></p>
+                        <p>Personnes formées<br><strong class="valeur" value="0" min="1" max="779">779</strong></p>
                     </div>
                     
                     <div id="progress9">
-                        <p>Demandeurs d'emploi<br><strong value="0" min="1" max="76">76%</strong></p>
+                        <p>Demandeurs d'emploi<br><strong class="valeur" value="0" min="1" max="76">76%</strong></p>
                     </div>
                     
                     <div id="progress11">
-                        <p>Jeunes<br><strong value="0" min="1" max="53">53%</strong></p>
+                        <p>Jeunes<br><strong class="valeur" value="0" min="1" max="53">53%</strong></p>
                     </div>
                     
                     <div id="progress13">
-                        <p>Niveau Bac ou inférieur<br><strong value="0" min="1" max="58">58%</strong></p>
+                        <p>Niveau Bac ou inférieur<br><strong class="valeur" value="0" min="1" max="58">58%</strong></p>
                     </div>
                     
                 </div>
@@ -102,15 +102,15 @@
                 <div class="chiffresPair">
 
                     <div id="progress8">
-                        <p>En cours de formation<br><strong value="0" min="1" max="335">335</strong></p>
+                        <p>En cours de formation<br><strong class="valeur" value="0" min="1" max="335">335</strong></p>
                     </div>
 
                     <div id="progress10">
-                        <p>Femmes<br><strong value="0" min="1" max="31">31%</strong></p>
+                        <p>Femmes<br><strong class="valeur" value="0" min="1" max="31">31%</strong></p>
                     </div>
 
                     <div id="progress12">
-                        <p>Seniors (45+)<br><strong value="0" min="1" max="6">6%</strong></p>
+                        <p>Seniors (45+)<br><strong class="valeur" value="0" min="1" max="6">6%</strong></p>
                     </div>
                         
                 </div>
@@ -121,7 +121,7 @@
              
              
                     <div id="progress14">
-                        <p>Personnes formées<br><strong value="0" min="1" max="78">78%</strong></p>
+                        <p>Personnes formées<br><strong class="valeur" value="0" min="1" max="78">78%</strong></p>
                     </div>
              
              
@@ -130,7 +130,7 @@
             <p>Activité de production solidaire employant d’ancien(ne)s Simplonien(ne)s pour réaliser des prestations (sites web, applications mobiles, maintenance, prototypage, objets connectés…) à destination d’ONG, d’acteurs de l’ESS et d’entreprises responsables :</p>
              
                     <div id="progress15">
-                        <p>Ex-Simplonien.ne.s en contrat<br><strong value="0" min="1" max="8">8</strong></p>
+                        <p>Ex-Simplonien.ne.s en contrat<br><strong class="valeur" value="0" min="1" max="8">8</strong></p>
              </div>
                        
              
