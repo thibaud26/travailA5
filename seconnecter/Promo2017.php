4295360b3ae383775d4f3a4700266840f873cb87
Alan <Alan.lazzarotto@hotmail.fr>
2017-06-07 10:26:43 +0200

    Ajout leger  ombrage sur les <h2>, et de quelques lignes de texte sur la page 'Qui_Finance', 'Partenaires' et 'Promo2017'

--- seconnecter/Promo2017.php
+++ seconnecter/Promo2017.php
@@ -19,129 +19,133 @@
     
     <section>
         <article>
-            <ul class="PROMO">
-                <li>
-                    <a>
-                    <img src="../Photos/Joffrey1.jpg" alt="Joffrey">
-                    <strong>Joffrey</strong>
-                    </a>
-                </li>
-                <li>
-                    <a>
-                    <img src="../Photos/Thibaud1.jpg" alt="Thibaud">
-                    <strong>Thibaud</strong>
-                    </a>
-                </li>
-                <li>
-                    <a>
-                    <img src="../Photos/Fanny1.jpg" alt="Fanny">
-                    <strong>Fanny</strong>
-                    </a>
-                </li>
-                <li>
-                    <a>
-                    <img src="../Photos/Alan1.jpg" alt="Alan">
-                    <strong>Alan</strong>
-                    </a>
-                </li>
-                <li>
-                    <a>
-                    <img src="../Photos/Stephane1.jpg" alt="Stéphane">
-                    <strong>Stéphane</strong>
-                    </a>
-                </li>
-                <li>
-                    <a>
-                    <img src="../Photos/BenoitLi1.jpg" alt="BenoitLi">
-                    <strong>Benoit Li</strong>
-                    </a>
-                </li>
-                <li>
-                    <a>
-                    <img src="../Photos/BenoitLa1.jpg" alt="BenoitLa">
-                    <strong>Benoit La</strong>
-                    </a>
-                </li>
-                <li>
-                    <a>
-                    <img src="../Photos/Fred1.jpg" alt="Fred">
-                    <strong>Fred</strong>
-                    </a>
-                </li>
-                <li>
-                    <a>
-                    <img src="../Photos/Jordan1.jpg" alt="Jordan">
-                    <strong>Jordan</strong>
-                    </a>
-                </li>
-                <li>
-                    <a>
-                    <img src="../Photos/Kevin1.jpg" alt="Kevin">
-                    <strong>Kevin</strong>
-                    </a>
-                </li>
-                <li>
-                    <a>
-                    <img src="../Photos/Lise-Claire1.jpg" alt="Lise-Claire">
-                    <strong>Lise</strong>
-                    </a>
-                </li>
-                <li>
-                    <a>
-                    <img src="../Photos/Mehdi1.jpg" alt="Mehdi">
-                    <strong>Mehdi</strong>
-                    </a>
-                </li>
-                <li>
-                    <a>
-                    <img src="../Photos/Mourad1.jpg" alt="Mourad">
-                    <strong>Mourad</strong>
-                    </a>
-                </li>
-                <li>
-                    <a>
-                    <img src="../Photos/Piotr1.jpg" alt="Piotr">
-                    <strong>Piotr</strong>
-                    </a>
-                </li>
-                <li>
-                    <a>
-                    <img src="../Photos/Nolwenn1.jpg" alt="Nolwenn">
-                    <strong>Nolwenn</strong>
-                    </a>
-                </li>
-                <li>
-                    <a>
-                    <img src="../Photos/Mylene1.jpg" alt="Mylene">
-                    <strong>Mylene</strong>
-                    </a>
-                </li>
-                <li>
-                    <a>
-                    <img src="../Photos/Nima1.jpg" alt="Nima">
-                    <strong>Nima</strong>
-                    </a>
-                </li>
-                <li>
-                    <a>
-                    <img src="../Photos/Quentin1.jpg" alt="Quentin">
-                    <strong>Quentin</strong>
-                    </a>
-                </li>
-                <li>
-                    <a>
-                    <img src="../Photos/Rita1.jpg" alt="Rita">
-                    <strong>Rita</strong>
-                    </a>
-                </li>
-                <li>
-                    <a>
-                    <img src="../Photos/Sylvain1.jpg" alt="Sylvain">
-                    <strong>Sylvain</strong>
-                    </a>
-                </li>
-                <li>
-                </li>
+            <h2>Pr<span>o</span>motion 2017</h2>
+                <ul>Voici la première promotion Devel<span>o</span>'Pont de 2017. Elle a commencée en mai et se terminera en Novembre de la même année. Elle est composé d'une vingtaine de personnes:<br><br>
+                (Petite astuce: si vous passez votre curseur sur chaque photos, vous pouvez voir le prénom de la personne, c'est cool hein !)  
+                </ul>
+                <ul class="PROMO">
+                    <li>
+                        <a>
+                        <img src="../Photos/Joffrey1.jpg" alt="Joffrey">
+                        <strong>Joffrey</strong>
+                        </a>
+                    </li>
+                    <li>
+                        <a>
+                        <img src="../Photos/Thibaud1.jpg" alt="Thibaud">
+                        <strong>Thibaud</strong>
+                        </a>
+                    </li>
+                    <li>
+                        <a>
+                        <img src="../Photos/Fanny1.jpg" alt="Fanny">
+                        <strong>Fanny</strong>
+                        </a>
+                    </li>
+                    <li>
+                        <a>
+                        <img src="../Photos/Alan1.jpg" alt="Alan">
+                        <strong>Alan</strong>
+                        </a>
+                    </li>
+                    <li>
+                        <a>
+                        <img src="../Photos/Stephane1.jpg" alt="Stéphane">
+                        <strong>Stéphane</strong>
+                        </a>
+                    </li>
+                    <li>
+                        <a>
+                        <img src="../Photos/BenoitLi1.jpg" alt="BenoitLi">
+                        <strong>Benoit Li</strong>
+                        </a>
+                    </li>
+                    <li>
+                        <a>
+                        <img src="../Photos/BenoitLa1.jpg" alt="BenoitLa">
+                        <strong>Benoit La</strong>
+                        </a>
+                    </li>
+                    <li>
+                        <a>
+                        <img src="../Photos/Fred1.jpg" alt="Fred">
+                        <strong>Fred</strong>
+                        </a>
+                    </li>
+                    <li>
+                        <a>
+                        <img src="../Photos/Jordan1.jpg" alt="Jordan">
+                        <strong>Jordan</strong>
+                        </a>
+                    </li>
+                    <li>
+                        <a>
+                        <img src="../Photos/Kevin1.jpg" alt="Kevin">
+                        <strong>Kevin</strong>
+                        </a>
+                    </li>
+                    <li>
+                        <a>
+                        <img src="../Photos/Lise-Claire1.jpg" alt="Lise-Claire">
+                        <strong>Lise</strong>
+                        </a>
+                    </li>
+                    <li>
+                        <a>
+                        <img src="../Photos/Mehdi1.jpg" alt="Mehdi">
+                        <strong>Mehdi</strong>
+                        </a>
+                    </li>
+                    <li>
+                        <a>
+                        <img src="../Photos/Mourad1.jpg" alt="Mourad">
+                        <strong>Mourad</strong>
+                        </a>
+                    </li>
+                    <li>
+                        <a>
+                        <img src="../Photos/Piotr1.jpg" alt="Piotr">
+                        <strong>Piotr</strong>
+                        </a>
+                    </li>
+                    <li>
+                        <a>
+                        <img src="../Photos/Nolwenn1.jpg" alt="Nolwenn">
+                        <strong>Nolwenn</strong>
+                        </a>
+                    </li>
+                    <li>
+                        <a>
+                        <img src="../Photos/Mylene1.jpg" alt="Mylene">
+                        <strong>Mylene</strong>
+                        </a>
+                    </li>
+                    <li>
+                        <a>
+                        <img src="../Photos/Nima1.jpg" alt="Nima">
+                        <strong>Nima</strong>
+                        </a>
+                    </li>
+                    <li>
+                        <a>
+                        <img src="../Photos/Quentin1.jpg" alt="Quentin">
+                        <strong>Quentin</strong>
+                        </a>
+                    </li>
+                    <li>
+                        <a>
+                        <img src="../Photos/Rita1.jpg" alt="Rita">
+                        <strong>Rita</strong>
+                        </a>
+                    </li>
+                    <li>
+                        <a>
+                        <img src="../Photos/Sylvain1.jpg" alt="Sylvain">
+                        <strong>Sylvain</strong>
+                        </a>
+                    </li>
+                    <li>
+                    </li>
             </ul>
         </article>
     </section>
