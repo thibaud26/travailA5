6ee20effae43c55b0cc40c1839e2728938626731
fanny26 <fannysterieuse2@hotmail.fr>
2017-06-07 09:12:31 +0200

    ajout css

--- seconnecter/Galerie.php
+++ seconnecter/Galerie.php
@@ -29,52 +29,201 @@
         
         <article>
             
-             <div class='galeriePhoto'>
-                 <input id="play" type="checkbox">
-                 <label for="play">Lancer Le Diapo</label>
-                    <div class="contenu_galerie">
-                        <div class="galerie">
-                            <img src="../Photos/1.jpg" alt>
-                            <img src="../Photos/2.jpg" alt>
-                            <img src="../Photos/3.jpg" alt>
-                            <img src="../Photos/4.jpg" alt>
-                            <img src="../Photos/5.jpg" alt>
-                            <img src="../Photos/6.jpg" alt>
-                            <img src="../Photos/7.jpg" alt>
-                            <img src="../Photos/8.jpg" alt>
-                            <img src="../Photos/9.jpg" alt>
-                            <img src="../Photos/10.jpg" alt>
-                            <img src="../Photos/11.jpg" alt>
-                            <img src="../Photos/12.jpg" alt>
-                            <img src="../Photos/13.jpg" alt>
-                            <img src="../Photos/14.jpg" alt>
-                            <img src="../Photos/15.jpg" alt>
-                            <img src="../Photos/16.jpg" alt>
-                            <img src="../Photos/17.jpg" alt>
-                            <img src="../Photos/18.jpg" alt>
-                            <img src="../Photos/19.jpg" alt>
-                            <img src="../Photos/1.jpg" alt>
-                            <img src="../Photos/2.jpg" alt>
-                            <img src="../Photos/3.jpg" alt>
-                            <img src="../Photos/4.jpg" alt>
-                            <img src="../Photos/5.jpg" alt>
-                            <img src="../Photos/6.jpg" alt>
-                            <img src="../Photos/7.jpg" alt>
-                            <img src="../Photos/8.jpg" alt>
-                            <img src="../Photos/9.jpg" alt>
-                            <img src="../Photos/10.jpg" alt>
-                            <img src="../Photos/11.jpg" alt>
-                            <img src="../Photos/12.jpg" alt>
-                            <img src="../Photos/13.jpg" alt>
-                            <img src="../Photos/14.jpg" alt>
-                            <img src="../Photos/15.jpg" alt>
-                            <img src="../Photos/16.jpg" alt>
-                            <img src="../Photos/17.jpg" alt>
-                            <img src="../Photos/18.jpg" alt>
-                            <img src="../Photos/19.jpg" alt>
-                        </div>
-                    </div>
-            </div>
+            <span id="sl_play" class="sl_command"></span>
+            <span id="sl_pause" class="sl_command"></span>
+ 
+            <span id="sl_i1" class="sl_command sl_i"></span>
+            <span id="sl_i2" class="sl_command sl_i"></span>
+            <span id="sl_i3" class="sl_command sl_i"></span>
+            <span id="sl_i4" class="sl_command sl_i"></span>
+            <span id="sl_i5" class="sl_command sl_i"></span>
+            <span id="sl_i6" class="sl_command sl_i"></span>
+            <span id="sl_i7" class="sl_command sl_i"></span>
+            <span id="sl_i8" class="sl_command sl_i"></span>
+            <span id="sl_i9" class="sl_command sl_i"></span>
+            <span id="sl_i11" class="sl_command sl_i"></span>
+            <span id="sl_i12" class="sl_command sl_i"></span>
+            <span id="sl_i13" class="sl_command sl_i"></span>
+            <span id="sl_i14" class="sl_command sl_i"></span>
+            <span id="sl_i15" class="sl_command sl_i"></span>
+            <span id="sl_i16" class="sl_command sl_i"></span>
+            <span id="sl_i17" class="sl_command sl_i"></span>
+            <span id="sl_i18" class="sl_command sl_i"></span>
+            <span id="sl_i19" class="sl_command sl_i"></span>
+            
+            
+<section id="slideshow">
+    
+    <a class="play_commands pause" href="#sl_pause" title="Maintain paused">Pause</a>
+    <a class="play_commands play" href="#sl_play" title="Play the animation">Play</a>
+    
+    
+    
+<a class="commands prev commands1" href="sl_i19" title="Aller à la dernière slide">&lt;</a>
+<a class="commands next commands1" href="sl_i2" title="Aller à la 2ème slide">&gt;</a>
+    
+<a class="commands prev commands2" href="sl_i1" title="Aller à la 1ère slide">&lt;</a>
+<a class="commands next commands2" href="sl_i3" title="Aller à la 3ème slide">&gt;</a>
+    
+<a class="commands prev commands3" href="sl_i2" title="Aller à la 2ème slide">&lt;</a>
+<a class="commands next commands3" href="sl_i4" title="Aller à la 4ème slide">&gt;</a>
+    
+<a class="commands prev commands4" href="sl_i3" title="Aller à la 3ème slide">&lt;</a>
+<a class="commands next commands4" href="sl_i5" title="Aller à la 5ème slide">&gt;</a>   
+
+<a class="commands prev commands5" href="sl_i4" title="Aller à la 4ème slide">&lt;</a>
+<a class="commands next commands5" href="sl_i6" title="Aller à la 6ème slide">&gt;</a>  
+    
+<a class="commands prev commands6" href="sl_i5" title="Aller à la 5ème slide">&lt;</a>
+<a class="commands next commands6" href="sl_i7" title="Aller à la 7ème slide">&gt;</a>  
+    
+<a class="commands prev commands7" href="sl_i6" title="Aller à la 6ème slide">&lt;</a>
+<a class="commands next commands7" href="sl_i8" title="Aller à la 8ème slide">&gt;</a> 
+    
+<a class="commands prev commands8" href="sl_i7" title="Aller à la 7ème slide">&lt;</a>
+<a class="commands next commands8" href="sl_i9" title="Aller à la 9ème slide">&gt;</a> 
+
+<a class="commands prev commands9" href="sl_i8" title="Aller à la 8ème slide">&lt;</a>
+<a class="commands next commands9" href="sl_i11" title="Aller à la 11ème slide">&gt;</a> 
+    
+<a class="commands prev commands10" href="sl_i9" title="Aller à la 9ème slide">&lt;</a>
+<a class="commands next commands10" href="sl_i12" title="Aller à la 12ème slide">&gt;</a>
+    
+<a class="commands prev commands11" href="sl_i9" title="Aller à la 11ème slide">&lt;</a>
+<a class="commands next commands11" href="sl_i12" title="Aller à la 13ème slide">&gt;</a>  
+    
+<a class="commands prev commands12" href="sl_i11" title="Aller à la 12ème slide">&lt;</a>
+<a class="commands next commands12" href="sl_i13" title="Aller à la 14ème slide">&gt;</a> 
+    
+<a class="commands prev commands13" href="sl_i12" title="Aller à la 13ème slide">&lt;</a>
+<a class="commands next commands13" href="sl_i14" title="Aller à la 15ème slide">&gt;</a> 
+    
+<a class="commands prev commands14" href="sl_i13" title="Aller à la 14ème slide">&lt;</a>
+<a class="commands next commands14" href="sl_i15" title="Aller à la 16ème slide">&gt;</a>    
+    
+<a class="commands prev commands15" href="sl_i14" title="Aller à la 15ème slide">&lt;</a>
+<a class="commands next commands15" href="sl_i16" title="Aller à la 17ème slide">&gt;</a>  
+    
+<a class="commands prev commands16" href="sl_i15" title="Aller à la 16ème slide">&lt;</a>
+<a class="commands next commands16" href="sl_i17" title="Aller à la 18ème slide">&gt;</a>     
+
+<a class="commands prev commands17" href="sl_i16" title="Aller à la 17ème slide">&gt;</a>  
+<a class="commands next commands17" href="sl_i18" title="Aller à la 19ème slide">&gt;</a>    
+
+<a class="commands prev commands18" href="sl_i17" title="Aller à la 17ème slide">&gt;</a>     
+<a class="commands next commands18" href="sl_i19" title="Aller à la 1ère slide">&gt;</a>
+
+<a class="commands prev commands19" href="sl_i18" title="Aller à la 17ème slide">&gt;</a>     
+<a class="commands next commands19" href="sl_i1" title="Aller à la 1ère slide">&gt;</a>    
+		
+	<div class="container">
+		<div class="c_slider"></div>
+		<div class="slider">
+			<figure>
+				<img src="../Photos/1.jpg" alt="" width="640" height="310" />
+				<figcaption>Photo De Groupe</figcaption>
+
+			</figure><!--
+			--><figure>
+				<img src="../Photos/2.jpg" alt="" width="640" height="310" />
+				<figcaption>Atelier Lego</figcaption>
+			</figure><!--
+			--><figure>
+				<img src="../Photos/3.jpg" alt="" width="640" height="310" />
+				<figcaption>Atelier Lego</figcaption>
+
+			</figure><!--
+			--><figure>
+				<img src="../Photos/4.jpg" alt="" width="640" height="310" />
+				<figcaption>Atelier Lego</figcaption>
+            </figure><!--
+			--><figure>
+				<img src="../Photos/5.jpg" alt="" width="640" height="310" />
+				<figcaption>Sortie Exposition</figcaption>
+            </figure><!--
+			--><figure>
+				<img src="../Photos/6.jpg" alt="" width="640" height="310" />
+				<figcaption>Sortie Exposition</figcaption>
+            </figure><!--
+			--><figure>
+				<img src="../Photos/7.jpg" alt="" width="640" height="310" />
+				<figcaption>Sortie Exposition</figcaption>
+            </figure><!--
+			--><figure>
+				<img src="../Photos/8.jpg" alt="" width="640" height="310" />
+				<figcaption>Sortie Exposition</figcaption>
+            </figure><!--
+			--><figure>
+				<img src="../Photos/9.jpg" alt="" width="640" height="310" />
+				<figcaption>Sortie Exposition</figcaption>
+            </figure><!--
+			--><figure>
+				<img src="../Photos/11.jpg" alt="" width="640" height="310" />
+				<figcaption>Sortie Exposition</figcaption>
+            </figure><!--
+			--><figure>
+				<img src="../Photos/12.jpg" alt="" width="640" height="310" />
+				<figcaption>Sortie Exposition</figcaption>
+            </figure><!--
+			--><figure>
+				<img src="../Photos/13.jpg" alt="" width="640" height="310" />
+				<figcaption>Dab Groupe</figcaption>
+            </figure><!--
+			--><figure>
+				<img src="../Photos/14.jpg" alt="" width="640" height="310" />
+				<figcaption>Au Boulot</figcaption>
+            </figure><!--
+			--><figure>
+				<img src="../Photos/15.jpg" alt="" width="640" height="310" />
+				<figcaption>Au Boulot</figcaption>
+            </figure><!--
+			--><figure>
+				<img src="../Photos/16.jpg" alt="" width="640" height="310" />
+				<figcaption>Au Boulot</figcaption>
+            </figure><!--
+			--><figure>
+				<img src="../Photos/17.jpg" alt="" width="640" height="310" />
+				<figcaption>Au Boulot</figcaption>
+            </figure><!--
+			--><figure>
+				<img src="../Photos/18.jpg" alt="" width="640" height="310" />
+				<figcaption>Au Boulot</figcaption>
+            </figure><!--
+			--><figure>
+				<img src="../Photos/19.jpg" alt="" width="640" height="310" />
+				<figcaption>Au Boulot</figcaption>
+			</figure>
+		</div>
+	</div>
+    
+    <span id="timeline"></span>
+    
+<ul class="dots_commands"><!--
+	--><li><a title="Afficher la slide 1" href="../Photos/1.jpg">Slide 1</a></li><!--
+	--><li><a title="Afficher la slide 2" href="../Photos/2.jpg">Slide 2</a></li><!--
+	--><li><a title="Afficher la slide 3" href="../Photos/3.jpg">Slide 3</a></li><!--
+	--><li><a title="Afficher la slide 4" href="../Photos/2.jpg">Slide 4</a></li><!--
+    --><li><a title="Afficher la slide 5" href="../Photos/5.jpg">Slide 5</a></li><!--
+	--><li><a title="Afficher la slide 6" href="../Photos/6.jpg">Slide 6</a></li><!--
+	--><li><a title="Afficher la slide 7" href="../Photos/7.jpg">Slide 7</a></li><!--
+	--><li><a title="Afficher la slide 8" href="../Photos/8.jpg">Slide 8</a></li><!--
+    --><li><a title="Afficher la slide 9" href="../Photos/9.jpg">Slide 9</a></li><!--
+	--><li><a title="Afficher la slide 11" href="../Photos/11.jpg">Slide 11</a></li><!--
+	--><li><a title="Afficher la slide 12" href="../Photos/12.jpg">Slide 12</a></li><!--
+	--><li><a title="Afficher la slide 13" href="../Photos/13.jpg">Slide 13</a></li><!--
+    --><li><a title="Afficher la slide 14" href="../Photos/14.jpg">Slide 14</a></li><!--
+	--><li><a title="Afficher la slide 15" href="../Photos/15.jpg">Slide 15</a></li><!--
+	--><li><a title="Afficher la slide 16" href="../Photos/16.jpg">Slide 16</a></li><!--
+	--><li><a title="Afficher la slide 17" href="../Photos/17.jpg">Slide 17</a></li><!--
+    --><li><a title="Afficher la slide 18" href="../Photos/18.jpg">Slide 18</a></li><!--
+	--><li><a title="Afficher la slide 19" href="../Photos/19.jpg">Slide 19</a></li>
+</ul>
+    
+    <a class="play_commands pause" href="#sl_pause" title="Maintain paused">Pause</a>
+    <a class="play_commands play" href="#sl_play" title="Play the animation">Play</a>
+		
+</section>
+            
         </article>
     
     </section>
